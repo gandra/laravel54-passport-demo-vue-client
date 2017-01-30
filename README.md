b6ce571e55149bb62eec54a4bd4146a5caf85e70
Dragan Mijatovic <gandra404@yahoo.com>
2017-01-31 00:02:29 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# laravel54-passport-demo-vue-client
+
+> A Vue.js client for laravel54-passport-demo-external-user
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# run unit tests
+npm run unit
+
+# run e2e tests
+npm run e2e
+
+# run all tests
+npm test
+```
+
+For detailed explanation on how things work, checkout the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <hello></hello>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello'
+
+export default {
+  name: 'app',
+  components: {
+    Hello
+  },
+  created () {
+    const postData = {
+      grant_type: 'password',
+      client_id: 2,
+      client_secret: 'zTAsKOickKnNHh0J9TzV3BkDgqDHUzrpkWSlAsLR',
+      username: 'aaa',
+      password: '1234',
+      scope: ''
+    }
+    this.$http.post('http://127.0.0.1:8000/oauth/token', postData)
+      .then(response => {
+        console.log(response)
+      })
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
